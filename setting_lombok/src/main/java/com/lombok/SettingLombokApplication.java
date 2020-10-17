14fcab18650214c97d66c1f35cb9f3763dfb081e
GoluKumar4024 <str.golu@gmail.com>
2020-10-17 12:17:36 +0530

    first commit in master

---
+++ setting_lombok/src/main/java/com/lombok/SettingLombokApplication.java
@@ -0,0 +1,13 @@
+package com.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingLombokApplication.class, args);
+	}
+
+}
